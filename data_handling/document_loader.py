1220ce9f3b219c6dd634b9b436c2f2c804f7828b
mattsmith7 <matthewtsmith007@gmail.com>
2024-02-24 21:46:57 -0600

    feat: document loader and embedder

---
+++ data_handling/document_loader.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import json
+
+#reads .csv file from data folder
+data = pd.read_csv('../data/exchange-formatted-content.csv', header=0)
+#converts .csv to json format
+jsonData = data.to_dict(orient='records')
+#opens new json file and dumps jsonData into it
+with open("exchange_content.json", "w") as file:
+    json.dump(jsonData, file)
