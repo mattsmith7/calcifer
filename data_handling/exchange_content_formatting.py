346c59294614d3091a2bae9f971e18166cf64d7f
mattsmith7 <matthewtsmith007@gmail.com>
2024-02-26 21:33:53 -0600

    feat: finish data reformatting

--- data_handling/exchange_content_formatting.py
+++ data_handling/exchange_content_formatting.py
@@ -5,14 +5,11 @@ with open("./data/exchange_content.json", "r") as file:
     data = json.load(file)
 
     for item in data:
+        if item['prices'] is None:
+            
+            item['prices'] = []
 
-        newItem = item["tags"][0].replace("}]", "")
 
-        item["tags"][0] = newItem
-
-        print(item["tags"][0])
-
-    
 with open("./data/exchange_content.json", "w") as file:
     json.dump(data, file)
 
