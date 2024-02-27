725b25228c84f0a25c44c7fb8f256ec6a39d14f9
mattsmith7 <matthewtsmith007@gmail.com>
2024-02-27 15:13:23 -0600

    feat: splitter class

--- data_handling/json_splitter.py
+++ data_handling/json_splitter.py
@@ -5,16 +5,32 @@ from langchain.text_splitter import RecursiveJsonSplitter
 
 from document_loader import JsonData
 
-splitter = RecursiveJsonSplitter()
+class SplitJson:
+    def __init__(self, maxChunk=2000):
+        self.maxChunk = maxChunk
 
-json_data = JsonData().retrieve_json_data()
+        # calls JsonData from data loader
+        self.json_data = JsonData().retrieve_json_data()
 
-# or a list of strings
-texts = splitter.split_text(json_data=json_data, convert_lists=True)
+        splitter = RecursiveJsonSplitter(max_chunk_size=self.maxChunk)
 
-pprint(texts[0])
-pprint(texts[1])
-pprint(texts[2])
+        # creates list of str splits from original json data
+        self.splits = splitter.split_text(json_data=self.json_data, convert_lists=True)
 
-print(len(texts))
-print([len(text) for text in texts])
+    # return list of all splits or specific one if index is given
+    def returnSplitsSample(self, index="all"):
+        if index == "all":
+            return self.splits
+        else:
+            return self.splits[index]
+        
+    # return number of total splits
+    def returnSplitsLength(self):
+        return len(self.splits)
+    
+    # return split lengths of count given or all if no count given
+    def returnSplitsSampleLengths(self, count="all"):
+        if count == "all":
+            return [len(split) for split in self.splits]
+        else:
+            return [len(split) for split in self.splits[:count]]
